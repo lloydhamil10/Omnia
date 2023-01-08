cc0d20a48e3ea465f551fd588a4ab2e14dc89bec
Lloyd Hamilton <loyd.hamilton@gmail.com>
2023-01-08 20:44:43 +0530

    Reduced token and added console logs

--- server/server.js
+++ server/server.js
@@ -17,19 +17,19 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello from Omnia!'
   })
 })
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
+console.log('Marker 1')
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 300, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
@@ -38,11 +38,11 @@ app.post('/', async (req, res) => {
     res.status(200).send({
       bot: response.data.choices[0].text
     });
-
+console.log('Marker 2')
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send(error || 'Something went wrong - Server');
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on Render'))
