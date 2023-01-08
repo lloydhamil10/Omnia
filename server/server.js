c22529afa6f1cb8c5d60640726e41c7213887a4d
Lloyd Hamilton <loyd.hamilton@gmail.com>
2023-01-08 14:21:26 +0530

    update server URL and fixed typos

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
-console.log(process.env.OPENAI_API_KEY)
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -47,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on onrender'))
